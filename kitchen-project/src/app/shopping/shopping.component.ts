c96c89e6272906172518b433e22af5176ee35cfa
PreethiKokila <kokilaz@gmail.com>
2020-09-13 21:28:16 -0400

    sunday commit

--- kitchen-project/src/app/shopping/shopping.component.ts
+++ kitchen-project/src/app/shopping/shopping.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping.component.css']
 })
 export class ShoppingComponent implements OnInit {
+  ingredients: Ingredient[] =[
+    new Ingredient('Mangoes', 10),
+    new Ingredient('Onions', 5),
+    new Ingredient('Lemons', 4),
+    new Ingredient('Bananas', 7),
+  ];
 
   constructor() { }
 
